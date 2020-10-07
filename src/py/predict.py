4506771e5560279489824bab3dc3732d405fc1f5
Juan Carlos Prieto <juanprietob@gmail.com>
2020-10-07 13:41:36 -0400

    BUG: Use the utils functions

--- src/py/predict.py
+++ src/py/predict.py
@@ -2,72 +2,12 @@
 import vtk
 import numpy as np
 import time
-import LinearSubdivisionFilter as lsf
 import itk
 import tensorflow as tf
 import argparse
 import os
 import post_process
-
-def Normalization(vtkdata):
-	polypoints = vtkdata.GetPoints()
-	
-	nppoints = []
-	for pid in range(polypoints.GetNumberOfPoints()):
-		spoint = polypoints.GetPoint(pid)
-		nppoints.append(spoint)
-	nppoints=np.array(nppoints)
-
-	npmean = np.mean(nppoints)
-	nppoints -= npmean
-	npscale = np.max(np.abs(np.reshape(nppoints,-1)))
-	nppoints /= npscale
-
-	for pid in range(polypoints.GetNumberOfPoints()):
-		vtkdata.GetPoints().SetPoint(pid, nppoints[pid])
-
-	return vtkdata, npmean, npscale
-
-def normalize_points(poly, radius):
-	polypoints = poly.GetPoints()
-	for pid in range(polypoints.GetNumberOfPoints()):
-		spoint = polypoints.GetPoint(pid)
-		spoint = np.array(spoint)
-		norm = np.linalg.norm(spoint)
-		spoint = spoint/norm * radius
-		polypoints.SetPoint(pid, spoint)
-	poly.SetPoints(polypoints)
-	return poly
-
-def normalize_vector(x):
-	return x/np.linalg.norm(x)
-
-def CreateIcosahedron(radius, sl):
-	icosahedronsource = vtk.vtkPlatonicSolidSource()
-	icosahedronsource.SetSolidTypeToIcosahedron()
-	icosahedronsource.Update()
-	icosahedron = icosahedronsource.GetOutput()
-	
-	subdivfilter = lsf.LinearSubdivisionFilter()
-	subdivfilter.SetInputData(icosahedron)
-	subdivfilter.SetNumberOfSubdivisions(sl)
-	subdivfilter.Update()
-
-	icosahedron = subdivfilter.GetOutput()
-	icosahedron = normalize_points(icosahedron, radius)
-
-	return icosahedron
-
-def CreatePlane(Origin,Point1,Point2,Resolution):
-	plane = vtk.vtkPlaneSource()
-	
-	plane.SetOrigin(Origin)
-	plane.SetPoint1(Point1)
-	plane.SetPoint2(Point2)
-	plane.SetXResolution(Resolution)
-	plane.SetYResolution(Resolution)
-	plane.Update()
-	return plane.GetOutput()
+from utils import *
 
 start_time = time.time()
 
