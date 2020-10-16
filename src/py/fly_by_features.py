8c7ae64f6922487bd1c3eff4ec5360da3ced3ebc
Juan Carlos Prieto <juanprietob@gmail.com>
2020-10-16 10:11:48 -0400

    ENH: fly_by_features in python
    This module may also take as input a tensorflow model to make predictions

---
+++ src/py/fly_by_features.py
@@ -0,0 +1,245 @@
+
+import os
+import tensorflow as tf
+import numpy as np
+import itk
+import vtk
+from vtk.util.numpy_support import vtk_to_numpy
+from vtk.util.numpy_support import numpy_to_vtk
+import argparse
+import time
+import glob
+import pandas
+import uuid 
+
+import LinearSubdivisionFilter as lsf
+from utils import * 
+
+# class ShapeAlignment(tf.keras.Model):
+
+# 	def __init__(self, dimension=2):
+# 		super(ShapeAlignment, self).__init__()
+
+# 		self.transform = tf.Variable(tf.eye(4), dtype=tf.float32)
+# 		self.loss = tf.keras.losses.MeanAbsoluteError()
+
+# 		# lr = tf.keras.optimizers.schedules.ExponentialDecay(1e-3, 100, 0.96, 1)
+# 		self.optimizer = tf.keras.optimizers.SGD(1e-3)
+
+# 	# @tf.function
+# 	def train_step(self, logits, target):
+
+# 		with tf.GradientTape() as tape:
+			
+# 			loss = self.loss(logits, target)
+			
+# 			var_list = self.trainable_variables
+
+# 			gradients = tape.gradient(loss, var_list)
+# 			self.optimizer.apply_gradients(zip(gradients, var_list))
+
+# 			return loss
+
+# 	def get_transform(self):
+# 		return self.transform.get_weights()
+	
+
+class FlyByGenerator():
+	def __init__(self, sphere, resolution, visualize=False):
+
+		renderer = vtk.vtkRenderer()
+		renderWindow = vtk.vtkRenderWindow()
+		renderWindow.AddRenderer(renderer)
+		renderWindow.SetSize(resolution, resolution)
+		renderWindow.OffScreenRenderingOn()
+
+		self.renderer = renderer
+		self.renderWindow = renderWindow
+		self.sphere = sphere
+		self.visualize = visualize
+
+	def removeActor(self, actor):
+		self.renderer.RemoveActor(actor)
+
+	def removeActors(self):
+		actors = self.renderer.GetActors()
+		actors.InitTraversal()
+		for i in range(actors.GetNumberOfItems()):
+			self.renderer.RemoveActor(actors.GetNextActor())
+
+	def addActor(self, actor):
+		self.renderer.AddActor(actor)
+
+	def getFlyBy(self):
+
+		sphere_points = self.sphere.GetPoints()
+		camera = self.renderer.GetActiveCamera()
+
+		if self.visualize:
+			self.renderWindow.OffScreenRenderingOff()
+			interactor = vtk.vtkRenderWindowInteractor()
+			interactor.SetRenderWindow(self.renderWindow)
+			interactor.Initialize()
+			interactor.Start()
+
+		img_seq = []
+		for i in range(sphere_points.GetNumberOfPoints()):
+
+			sphere_point = sphere_points.GetPoint(i)
+			sphere_point_v = normalize_vector(sphere_point)
+
+			if(abs(sphere_point_v[2]) != 1):
+				camera.SetViewUp(0, 0, -1)
+			elif(sphere_point_v[2] == 1):
+				camera.SetViewUp(1, 0, 0)
+			elif(sphere_point_v[2] == -1):
+				camera.SetViewUp(-1, 0, 0)
+
+			camera.SetPosition(sphere_point[0], sphere_point[1], sphere_point[2])
+			camera.SetFocalPoint(0, 0, 0)
+			
+			self.renderer.ResetCameraClippingRange()
+			# self.renderWindow.Render()
+
+			windowToImageN = vtk.vtkWindowToImageFilter()
+			windowToImageN.SetInputBufferTypeToRGB()
+			windowToImageN.SetInput(self.renderWindow)
+			windowToImageN.Update()
+
+			windowFilterZ = vtk.vtkWindowToImageFilter()
+			windowFilterZ.SetInputBufferTypeToZBuffer()
+			windowFilterZ.SetInput(self.renderWindow)
+
+			scalez = vtk.vtkImageShiftScale()
+			scalez.SetOutputScalarTypeToDouble();
+			scalez.SetInputConnection(windowFilterZ.GetOutputPort());
+			scalez.SetShift(0);
+			scalez.SetScale(-1);
+			scalez.Update()
+			
+			img_o = windowToImageN.GetOutput()
+			img_z = scalez.GetOutput()
+			
+			img_o_np = vtk_to_numpy(img_o.GetPointData().GetScalars())
+			scalez_np = vtk_to_numpy(img_z.GetPointData().GetScalars())
+			scalez_np = np.abs(scalez_np).reshape([-1, 1])
+
+			img_np = np.multiply(img_o_np, scalez_np)
+
+			img_seq.append(img_np.reshape([d for d in img_o.GetDimensions() if d != 1] + [img_o.GetNumberOfScalarComponents()]))
+
+		return np.array(img_seq)
+
+def main(args):
+
+	filenames = []
+
+	if(args.surf):
+		fobj = {}
+		fobj["surf"] = args.surf
+		fobj["out"] = args.out
+		filenames.append(fobj)
+			
+	else:
+		surf_filenames = []
+
+		if(args.dir):
+			replace_dir_name = args.dir
+			normpath = os.path.normpath("/".join([args.dir, '**', '*']))
+			for surf in glob.iglob(normpath, recursive=True):
+				if os.path.isfile(surf) and True in [ext in surf for ext in [".vtk", ".obj", ".stl"]]:
+					surf_filenames.append(os.path.realpath(surf))
+			
+		elif(args.csv):
+			replace_dir_name = args.csv_root_path
+			with open(args.csv) as csvfile:
+				df = pandas.read_csv(csvfile)
+
+			for index, row in df.iterrows():
+				surf_filenames.append(row["surf"])
+
+		for surf in surf_filenames:
+			fobj = {}
+			fobj["surf"] = surf
+
+			dir_filename = os.path.splitext(surf.replace(replace_dir_name, ''))[0] +  ".nrrd"
+			fobj["out"] = os.path.normpath("/".join([args.out, dir_filename]))
+
+			if(args.uuid):
+				fobj["out"] = fobj["out"].replace(".nrrd", "-" + str(uuid.uuid1()).split('-')[0] + ".nrrd")
+
+			if not os.path.exists(os.path.dirname(fobj["out"])):
+				os.makedirs(os.path.dirname(fobj["out"]))
+
+			if args.ow or not os.path.exists(fobj["out"]):
+				filenames.append(fobj)
+
+	if(args.subdivision):
+		sphere = CreateIcosahedron(args.radius, args.subdivision)
+	else:
+		sphere = CreateSpiral(args.radius, args.spiral, args.turns)
+
+	model = None
+	if args.model is not None:
+		model = tf.keras.models.load_model(args.model, custom_objects={'tf': tf})
+		model.summary()
+
+	flyby = FlyByGenerator(sphere, args.resolution, args.visualize)
+
+	for fobj in filenames:
+
+		surf_actor = GetUnitActor(fobj["surf"], args.random_rotation, True)
+		
+		if surf_actor is not None:
+			flyby.addActor(surf_actor)
+			
+			out_np = flyby.getFlyBy()
+
+			if model is not None:
+				out_np = model.predict(out_np)
+
+			out_img = GetImage(out_np)
+
+			print("Writing:", fobj["out"])
+			writer = itk.ImageFileWriter.New(FileName=fobj["out"], Input=out_img)
+			writer.UseCompressionOn()
+			writer.Update()
+
+		flyby.removeActors()
+
+
+if __name__ == '__main__':
+	start_time = time.time()
+
+	parser = argparse.ArgumentParser(description='Predict an input with a trained neural network', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+	input_group = parser.add_argument_group('Input parameters')
+	input_params = input_group.add_mutually_exclusive_group(required=True)
+	input_params.add_argument('--surf', type=str, help='Target surface/mesh')
+	input_params.add_argument('--dir', type=str, help='Input directory with 3D models')
+	input_params.add_argument('--csv', type=str, help='Input csv with column "surf"')
+
+	input_group.add_argument('--csv_root_path', type=str, help='CSV rooth path for replacement', default="")
+	input_group.add_argument('--model', type=str, help='Directory with saved model', default=None)
+	input_group.add_argument('--random_rotation', type=bool, help='Apply a random rotation', default=False)
+
+	sphere_params = parser.add_argument_group('Sampling parameters')
+	sphere_params_sampling = sphere_params.add_mutually_exclusive_group(required=True)
+	sphere_params_sampling.add_argument('--subdivision', type=int, help='Number of subdivisions for icosahedron')
+	sphere_params_sampling.add_argument('--spiral', type=int, help='Number of samples along the spherical spiral')
+
+	sphere_params.add_argument('--turns', type=int, default=4, help='Number of spiral turns')
+	sphere_params.add_argument('--resolution', type=int, help='Image resolution', default=256)
+	sphere_params.add_argument('--radius', type=float, help='Radius of the sphere for the view points', default=4)
+
+	visu_params = parser.add_argument_group('Visualize')
+	visu_params.add_argument('--visualize', type=int, default=0, help='Visualize the sampling')
+
+	output_params = parser.add_argument_group('Output parameters')
+	output_params.add_argument('--out', type=str, help='Output filename or directory', default="out.nrrd")
+	output_params.add_argument('--uuid', type=bool, help='Use uuid to name the outputs', default=False)
+	output_params.add_argument('--ow', type=int, help='Overwrite outputs', default=1)
+
+	args = parser.parse_args()
+
+	main(args)
--- src/py/utils.py
+++ src/py/utils.py
@@ -1,6 +1,11 @@
 import vtk
 import LinearSubdivisionFilter as lsf
 import numpy as np
+import math 
+import os
+import sys
+import itk
+from readers import OFFReader
 
 def Normalization(vtkdata):
 	polypoints = vtkdata.GetPoints()
@@ -50,6 +55,45 @@ def CreateIcosahedron(radius, sl):
 
 	return icosahedron
 
+def CreateSpiral(sphereRadius=4, numberOfSpiralSamples=64, numberOfSpiralTurns=4):
+	
+    sphere = vtk.vtkPolyData()
+    sphere_points = vtk.vtkPoints()
+    lines = vtk.vtkCellArray()
+    vertices = vtk.vtkCellArray()
+
+    c = 2.0*float(numberOfSpiralTurns)
+    prevPid = -1
+
+    for i in range(numberOfSpiralSamples):
+      p = [0, 0, 0]
+      #angle = i * 180.0/numberOfSpiralSamples * math.pi/180.0
+      angle = (i*math.pi)/numberOfSpiralSamples
+      p[0] = sphereRadius * math.sin(angle)*math.cos(c*angle)
+      p[1] = sphereRadius * math.sin(angle)*math.sin(c*angle)
+      p[2] = sphereRadius * math.cos(angle)
+
+      pid = sphere_points.InsertNextPoint(p)
+      
+      if(prevPid != -1):
+        line = vtk.vtkLine()
+        line.GetPointIds().SetId(0, prevPid)
+        line.GetPointIds().SetId(1, pid)
+        lines.InsertNextCell(line)
+
+      prevPid = pid
+
+      vertex = vtk.vtkVertex()
+      vertex.GetPointIds().SetId(0, pid)
+
+      vertices.InsertNextCell(vertex)
+    
+    sphere.SetVerts(vertices)
+    sphere.SetLines(lines)
+    sphere.SetPoints(sphere_points)
+
+    return sphere
+
 def CreatePlane(Origin,Point1,Point2,Resolution):
 	plane = vtk.vtkPlaneSource()
 	
@@ -60,3 +104,190 @@ def CreatePlane(Origin,Point1,Point2,Resolution):
 	plane.SetYResolution(Resolution)
 	plane.Update()
 	return plane.GetOutput()
+
+def ReadSurf(fileName):
+
+	print("Reading:", fileName)
+
+	fname, extension = os.path.splitext(fileName)
+	extension = extension.lower()
+	if extension == ".vtk":
+		reader = vtk.vtkPolyDataReader()
+		reader.SetFileName(fileName)
+		reader.Update()
+		surf = reader.GetOutput()
+	elif extension == ".stl":
+		reader = vtk.vtkSTLReader()
+		reader.SetFileName(fileName)
+		reader.Update()
+		surf = reader.GetOutput()
+	elif extension == ".off":
+		reader = OFFReader()
+		reader.SetFileName(fileName)
+		reader.Update()
+		surf = reader.GetOutput()
+	elif extension == ".obj":
+		if os.path.exists(fname + ".mtl"):
+			obj_import = vtk.vtkOBJImporter()
+			obj_import.SetFileName(fileName)
+			obj_import.SetFileNameMTL(fname + ".mtl")
+			textures_path = os.path.normpath(os.path.dirname(fname) + "/../images")
+			if os.path.exists(textures_path):
+				obj_import.SetTexturePath(textures_path)
+			obj_import.Read()
+
+			actors = obj_import.GetRenderer().GetActors()
+			actors.InitTraversal()
+			append = vtk.vtkAppendPolyData()
+
+			for i in range(actors.GetNumberOfItems()):
+				surfActor = actors.GetNextActor()
+				append.AddInputData(surfActor.GetMapper().GetInputAsDataSet())
+			
+			append.Update()
+			surf = append.GetOutput()
+			
+		else:
+			reader = vtk.vtkOBJReader()
+			reader.SetFileName(fileName)
+			reader.Update()
+			surf = reader.GetOutput()
+
+	return surf
+
+def GetActor(surf):
+	surfMapper = vtk.vtkPolyDataMapper()
+	surfMapper.SetInputData(surf)
+
+	surfActor = vtk.vtkActor()
+	surfActor.SetMapper(surfMapper)
+
+	return surfActor
+
+def RotateSurf(surf, rotationAngle, rotationVector):
+	print("angle:", rotationAngle, "vector:", rotationVector)
+	transform = vtk.vtkTransform()
+	transform.RotateWXYZ(rotationAngle, rotationVector[0], rotationVector[1], rotationVector[2]);
+
+	transformFilter = vtk.vtkTransformPolyDataFilter()
+	transformFilter.SetTransform(transform)
+	transformFilter.SetInputData(surf)
+	transformFilter.Update()
+	return transformFilter.GetOutput()
+
+def GetUnitActor(fileName, random_rotation=False, normal_shaders=True):
+
+	try:
+
+		surf = ReadSurf(fileName)
+
+		surf, surf_mean, surf_scale = Normalization(surf)
+
+		if(random_rotation):
+			rotationVector = np.random.random(3)*2.0 - 1.0
+			rotationVector = rotationVector/np.linalg.norm(rotationVector)
+			rotationAngle = np.random.random()*360.0
+			surf = RotateSurf(surf, rotationAngle, rotationVector)
+
+		if(normal_shaders):
+			normals = vtk.vtkPolyDataNormals()
+			normals.SetInputData(surf);
+			normals.ComputeCellNormalsOff();
+			normals.ComputePointNormalsOn();
+			# normals.AutoOrientNormalsOn();
+			normals.SplittingOff();
+			normals.Update()
+			surf = normals.GetOutput()
+
+		# mapper
+		surfActor = GetActor(surf)
+
+		if(normal_shaders):
+
+			sp = surfActor.GetShaderProperty();
+			sp.AddVertexShaderReplacement(
+				"//VTK::Normal::Dec",
+				True,
+				"//VTK::Normal::Dec\n" + 
+				"  varying vec3 myNormalMCVSOutput;\n",
+				False
+			)
+
+			sp.AddVertexShaderReplacement(
+				"//VTK::Normal::Impl",
+				True,
+				"//VTK::Normal::Impl\n" +
+				"  myNormalMCVSOutput = normalMC;\n",
+				False
+			)
+
+			sp.AddVertexShaderReplacement(
+				"//VTK::Color::Impl",
+				True, "VTK::Color::Impl\n", False)
+
+			sp.ClearVertexShaderReplacement("//VTK::Color::Impl", True)
+
+			sp.AddFragmentShaderReplacement(
+				"//VTK::Normal::Dec",
+				True,
+				"//VTK::Normal::Dec\n" + 
+				"  varying vec3 myNormalMCVSOutput;\n",
+				False
+			)
+
+			sp.AddFragmentShaderReplacement(
+				"//VTK::Normal::Impl",
+				True,
+				"//VTK::Normal::Impl\n" +
+				"  diffuseColor = myNormalMCVSOutput*0.5f + 0.5f;\n",
+				False
+			)
+
+		return surfActor
+	except Exception as e:
+		print(e, file=sys.stderr)
+		return None
+
+def GetImage(img_np):
+	img_np_shape = np.shape(img_np)
+	ComponentType = itk.ctype('float')
+
+	Dimension = img_np.ndim - 1
+	PixelDimension = img_np.shape[-1]
+	print("Dimension:", Dimension, "PixelDimension:", PixelDimension)
+
+	if Dimension == 1:
+		OutputImageType = itk.VectorImage[ComponentType, 2]
+	else:
+		OutputImageType = itk.VectorImage[ComponentType, Dimension]
+	
+	out_img = OutputImageType.New()
+	out_img.SetNumberOfComponentsPerPixel(PixelDimension)
+
+	size = itk.Size[OutputImageType.GetImageDimension()]()
+	size.Fill(1)
+	
+	prediction_shape = list(img_np.shape[0:-1])
+	prediction_shape.reverse()
+
+	if Dimension == 1:
+		size[1] = prediction_shape[0]
+	else:
+		for i, s in enumerate(prediction_shape):
+			size[i] = s
+
+	index = itk.Index[OutputImageType.GetImageDimension()]()
+	index.Fill(0)
+
+	RegionType = itk.ImageRegion[OutputImageType.GetImageDimension()]
+	region = RegionType()
+	region.SetIndex(index)
+	region.SetSize(size)
+
+	out_img.SetRegions(region)
+	out_img.Allocate()
+
+	out_img_np = itk.GetArrayViewFromImage(out_img)
+	out_img_np.setfield(img_np.reshape(out_img_np.shape), out_img_np.dtype)
+
+	return out_img
