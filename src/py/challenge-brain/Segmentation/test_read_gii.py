06d9c302dfd1a2a7e1a9d1a7be39c83417b6c741
MathieuLeclercq <m47h1eu.leclercq@gmail.com>
2022-05-13 13:24:12 -0400

    enh

--- src/py/challenge-brain/Segmentation/test_read_gii.py
+++ src/py/challenge-brain/Segmentation/test_read_gii.py
@@ -7,11 +7,17 @@ sys.path.insert(0,'../..')
 import utils
 import vtk
 from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk, numpy_to_vtkIdTypeArray
+import torch
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
 
-
+# rendering components
+from pytorch3d.renderer import (
+    FoVPerspectiveCameras, look_at_view_transform, look_at_rotation, 
+    RasterizationSettings, MeshRenderer, MeshRasterizer, BlendParams,
+    SoftSilhouetteShader, HardPhongShader, SoftPhongShader, AmbientLights, PointLights, TexturesUV, TexturesVertex,
+)
 
 class BrainDataset(Dataset):
 	def __init__(self,np_split):
@@ -22,7 +28,7 @@ class BrainDataset(Dataset):
 
 	def __getitem__(self,idx):
 		data_dir = '/CMF/data/geometric-deep-learning-benchmarking/Data/Segmentation/Native_Space'
-		item = self.np_split[idx]
+		item = self.np_split[idx][0]
 
 		# for now just try with Left
 		path_features = f'{data_dir}/segmentation_native_space_features/{item}_L.shape.gii'
@@ -30,18 +36,45 @@ class BrainDataset(Dataset):
 		data_features = gifti.loadGiftiVertexData(path_features)
 		data_labels = gifti.loadGiftiVertexData(path_labels)
 
+		return data_features,data_labels
+
 def main():
+
+	# Set the cuda device 
+	if torch.cuda.is_available():
+	    device = torch.device("cuda:0")
+	    torch.cuda.set_device(device)
+	else:
+	    device = torch.device("cpu")
+	# Initialize a perspective camera.
+	cameras = FoVPerspectiveCameras(device=device)
+
+
+	# We will also create a Phong renderer. This is simpler and only needs to render one face per pixel.
+	raster_settings = RasterizationSettings(
+	    image_size=224, 
+	    blur_radius=0, 
+	    faces_per_pixel=1, 
+	)
+	# We can add a point light in front of the object. 
+
+	# lights = AmbientLights(device=device)
+	lights = PointLights(device=device)
+	rasterizer = MeshRasterizer(
+	        cameras=cameras, 
+	        raster_settings=raster_settings
+	    )
+	phong_renderer = MeshRenderer(
+	    rasterizer=rasterizer,
+	    shader=HardPhongShader(device=device, cameras=cameras)
+	)
+
+
 	path_features = '/CMF/data/geometric-deep-learning-benchmarking/Data/Segmentation/Native_Space/segmentation_native_space_features/sub-CC00062XX05_ses-13801_R.shape.gii'
 	path_ico = '/CMF/data/geometric-deep-learning-benchmarking/Icospheres/ico-1.surf.gii'
-	# vertex_data = gifti.loadGiftiVertexData(path_ico)  # tuple[0] : gifti image, [1] : list of len dim0
+
 	#mesh = gifti.loadGiftiMesh(path_ico)
 
-	"""
-	# Load icosphere mesh
-	Mesh = gifti.GiftiMesh(path_ico)
-	# Add features data
-	Mesh.loadVertexData(path_features)
-	"""
 	# load icosahedron
 	surf = nib.load(path_ico)
 
@@ -49,7 +82,6 @@ def main():
 	coords = surf.agg_data('pointset')
 	triangles = surf.agg_data('triangle')
 	nb_triangles = len(triangles)
-	ic(nb_triangles)
 	connectivity = triangles.reshape(nb_triangles*3,1) # 3 points per triangle
 	connectivity = np.int64(connectivity)	
 	offsets = [3*i for i in range (nb_triangles)]
@@ -72,14 +104,21 @@ def main():
 	# test: write surface
 	# utils.Write(ico_polydata,"/home/leclercq/Documents/TOEIJT.vtk")
 
+	# convert ico verts / faces to tensor
+	ico_verts = torch.from_numpy(coords)
+	ico_faces = torch.from_numpy(triangles)
+
+
 	# load train / test splits
 	train_split_path = '/CMF/data/geometric-deep-learning-benchmarking/Train_Val_Test_Splits/Segmentation/M-CRIB-S_train_TEA.npy'
 	val_split_path = '/CMF/data/geometric-deep-learning-benchmarking/Train_Val_Test_Splits/Segmentation/M-CRIB-S_val_TEA.npy'
 	train_split = np.load(train_split_path)
 	val_split = np.load(val_split_path)
-	
 	train_data = BrainDataset(train_split)
 	val_dataset = BrainDataset(val_split)
+	train_item, val_item = train_data[5]
+
+
 
 
 
