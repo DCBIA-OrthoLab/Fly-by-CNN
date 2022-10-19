fdfe9ac6f22686dc352d91f8ad907ca91d78d368
Juan Carlos Prieto <juanprietob@gmail.com>
2022-10-19 00:59:14 -0400

    BUG: fix image logger

--- src/py/challenge-teeth/teeth_logger.py
+++ src/py/challenge-teeth/teeth_logger.py
@@ -28,14 +28,14 @@ class TeethNetImageLogger(Callback):
                     y = torch.take(YF, PF).to(torch.int64)*(PF >= 0) # YF=input, pix_to_face=index. shape of y = shape of pix_to_face
                     x = torch.argmax(x, dim=2, keepdim=True)
                     
-                    grid_X = torchvision.utils.make_grid(X[:, 0:num_images, 0:3, :, :])#Grab the first image, RGB channels only, X, Y. The time dimension is on dim=1
+                    grid_X = torchvision.utils.make_grid(X[0, 0:num_images, 0:3, :, :])#Grab the first image, RGB channels only, X, Y. The time dimension is on dim=1
                     trainer.logger.experiment.add_image('X_normals', grid_X, 0)
 
-                    grid_X = torchvision.utils.make_grid(X[:, 0:num_images, 3:, :, :])#Grab the depth map. The time dimension is on dim=1
+                    grid_X = torchvision.utils.make_grid(X[0, 0:num_images, 3:, :, :])#Grab the depth map. The time dimension is on dim=1
                     trainer.logger.experiment.add_image('X_depth', grid_X, 0)
                     
-                    grid_x = torchvision.utils.make_grid(x[:, 0:num_images, 0:1, :, :]/pl_module.out_channels)# The time dimension is on dim 1 grab only the first one
+                    grid_x = torchvision.utils.make_grid(x[0, 0:num_images, 0:1, :, :]/pl_module.out_channels)# The time dimension is on dim 1 grab only the first one
                     trainer.logger.experiment.add_image('x', grid_x, 0)
 
-                    grid_y = torchvision.utils.make_grid(y[:, 0:num_images, :, :, :]/pl_module.out_channels)# The time dimension here is swapped after the permute and is on dim=2. It will grab the first image
+                    grid_y = torchvision.utils.make_grid(y[0, 0:num_images, :, :, :]/pl_module.out_channels)# The time dimension here is swapped after the permute and is on dim=2. It will grab the first image
                     trainer.logger.experiment.add_image('Y', grid_y, 0)
